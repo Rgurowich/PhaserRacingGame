8ca8d62c93abcba277d6efeee844cfeba67ec6bb
Rgurowich <ross.gurowich@students.plymouth.ac.uk>
2018-01-06 20:25:17 +0000

    Login system and database
    login system now working with hosted online database. Player can not shoot bullets that collide with other players

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var mongoDB = require('mongojs');
+var accountDB = mongoDB("mongodb://rgurowich:Balota321@ds245687.mlab.com:45687/webscriptingdatabase",["UserAccounts"]);
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
@@ -29,6 +31,9 @@ var Entity = function() {
     self.x += self.spdX;
     self.y += self.spdY;
   }
+  self.getDistance = function(pt){
+    return Math.sqrt(Math.pow(self.x-pt.x,2) + Math.pow(self.y-pt.y,2));
+  }
   return self;
 }
 //player attributes//
@@ -40,12 +45,23 @@ var Player = function(id) {
   self.pressingLeft = false;
   self.pressingUp = false;
   self.pressingDown = false;
+  self.pressingClick = false;
+  self.mouseAngle = 0;
   self.maxSpeed = 10;
   //override for speed and update//
   var super_update = self.update;
   self.update = function() {
     self.updateSpd();
     super_update();
+
+    if(self.pressingClick){
+      self.fireShell(self.mouseAngle);
+    }
+  }
+  self.fireShell = function(angle){
+    var s = Shell(self.id,angle);
+    s.x = self.x;
+    s.y = self.y;
   }
 
   self.updateSpd = function() {
@@ -79,6 +95,10 @@ Player.onConnect = function(socket) {
       player.pressingUp = data.state;
     else if (data.inputId === 'down')
       player.pressingDown = data.state;
+    else if (data.inputId === 'firing')
+      player.pressingClick = data.state;
+    else if (data.inputId === 'mouseAngle')
+      player.mouseAngle = data.state;
   });
 }
 Player.onDisconnect = function(socket) {
@@ -98,11 +118,12 @@ Player.update = function() {
   return pack;
 }
 
-var Shell = function(angle){
+var Shell = function(parent, angle){
     var self = Entity();
     self.id = Math.random();
     self.spdX = Math.cos(angle/180*Math.PI) * 10;
     self.spdY = Math.sin(angle/180*Math.PI) * 10;
+    self.parent = parent;
     self.timer = 0;
     self.toRemove = false;
     var super_update = self.update;
@@ -110,6 +131,13 @@ var Shell = function(angle){
         if(self.timer++ > 100)
             self.toRemove = true;
         super_update();
+
+        for(var i in Player.list){
+          var p = Player.list[i];
+          if(self.getDistance(p) < 32 && self.parent !== p.id){
+            self.toRemove = true;
+          }
+        }
     }
     Shell.list[self.id] = self;
     return self;
@@ -117,14 +145,13 @@ var Shell = function(angle){
 Shell.list = {};
 
 Shell.update = function(){
-    if(Math.random() < 0.1){
-        Shell(Math.random()*360);
-    }
-
     var pack = [];
     for(var i in Shell.list){
         var shell = Shell.list[i];
         shell.update();
+        if(shell.toRemove){
+          delete Shell.list[i];
+        }
         pack.push({
             x:shell.x,
             y:shell.y,
@@ -132,19 +159,98 @@ Shell.update = function(){
     }
     return pack;
 }
+
+var DEBUG = true;
+
+var USERS = {
+  "ross":"cat",
+  "rosss":"catt",
+  "rossss":"cattt",
+}
+
+var isValidPassword = function(data,cb) {
+  accountDB.UserAccounts.find({username:data.username,password:data.password},function(error,res){
+    if(res.length > 0){
+      cb(true);
+    } else {
+      cb(false);
+    }
+  });
+  // setTimeout(function(){
+  //   cb(USERS[data.username] === data.password);
+  // },10);
+}
+var isUsernameValid = function(data,cb) {
+  accountDB.UserAccounts.find({username:data.username},function(error,res){
+    if(res.length > 0){
+      cb(true);
+    } else {
+      cb(false);
+    }
+  });
+  // setTimeout(function(){
+  //   cb (USERS[data.username]);
+  // },10);
+}
+var addUser = function(data,cb) {
+  accountDB.UserAccounts.insert({username:data.username,password:data.password},function(error){
+  cb();
+  });
+  // setTimeout(function(){
+  //   (USERS[data.username] = data.password);
+  //   cb();
+  // },10);
+}
+
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket) {
   socket.id = Math.random();
   SOCKET_LIST[socket.id] = socket;
 
-  //Calls for new player//
-  Player.onConnect(socket);
+  socket.on('logIn', function(data) {
+    isValidPassword(data,function(res){
+      if(res){
+        //Calls for new player once they have loged in//
+        Player.onConnect(socket);
+        socket.emit('logInResponse', {success:true});
+      } else {
+        socket.emit('logInResponse', {success:false});
+      }
+    });
+  });
+
+  socket.on('signUp',function(data){
+         isUsernameValid(data,function(res){
+             if(res){
+                 socket.emit('signUpResponse',{success:false});
+             } else {
+                 addUser(data,function(){
+                     socket.emit('signUpResponse',{success:true});
+                 });
+             }
+         });
+     });
+
 
   console.log('socket connection');
   socket.on('disconnect', function() {
     delete SOCKET_LIST[socket.id];
     Player.onDisconnect(socket);
   });
+  socket.on('sendTxtToServer', function(data) {
+    var playerName = ("" + socket.id).slice(2,7);
+    for(var i in SOCKET_LIST){
+      SOCKET_LIST[i].emit('addIngameText',playerName + ': ' + data);
+    }
+  });
+  socket.on('evalServer', function(data) {
+    if(!DEBUG){
+      return;
+    }
+    var res = eval(data);
+    socket.emit('evalAnswer', res);
+  });
+
 });
 
 setInterval(function() {
